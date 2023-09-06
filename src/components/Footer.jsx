6acb8a39313ed77d1a12e1a7429fc5bc69ea9e9b
sijju <karthik.msv1234@gmail.com>
2023-09-07 01:41:02 +0530

    minor changes to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,46 +1,48 @@
 
 import { motion } from "framer-motion"
-import { FormContainer, FormInput, FormLabel } from "../styles/Footer.styled"
-import { PaddingContainer,FlexContainer,Heading, BlueText, Button } from "../styles/Global.styled"
+
+import { PaddingContainer,FlexContainer,Heading, BlueText,IconContainer, Link } from "../styles/Global.styled"
 import { fadeInBottomvariant } from "../utils/Variants"
+import { BsGithub, BsInstagram, BsLinkedin, BsTwitter } from "react-icons/bs"
+import {FiMail} from 'react-icons/fi'
 const Footer = () => {
   return (
     <PaddingContainer id="Contact" top='5%' bottom='5%'>
         <Heading as={motion.h2} variants={fadeInBottomvariant} initial='hidden' whileInView='visible' size='h2' align='center'>Connect <BlueText>with me</BlueText> </Heading> 
         <PaddingContainer top='3rem'>
-            <FlexContainer justify='center'>
-                <FormContainer as={motion.form}  variants={fadeInBottomvariant} initial='hidden' whileInView='visible'>
-                    <PaddingContainer bottom='2rem'>
-                        <FormLabel>
-                            Name
-                        </FormLabel>
-                        
-                        <FormInput type='text' placeholder="Enter your name here..." />
-                        
-                    </PaddingContainer>
-                    <PaddingContainer bottom='2rem'>
-                        <FormLabel>
-                            Email
-                        </FormLabel>
-                        
-                        <FormInput type='text' placeholder="Enter your email here..." />
-                        
-                    </PaddingContainer>
-                    <PaddingContainer bottom='2rem'>
-                        <FormLabel>
-                            Message
-                        </FormLabel>
-                        
-                        <FormInput as='textarea' placeholder="Enter your name here..." />
-                        
-                    </PaddingContainer>
-                    <FlexContainer justify='center' responsiveFlex>
-                        <Button>Send Message</Button>
-                    </FlexContainer>
+            <FlexContainer justify='center' gap='2rem' responsiveFlex>
+            <IconContainer  size='1.5rem'>
+                   <Link href='https://www.linkedin.com/in/karthik-msv-833366112/' target='blank'> 
+                    <BsLinkedin />
+                   </Link>
+                  </IconContainer>
+                  <IconContainer color='white' size='1.5rem'>
+                    <Link href='https://instagram.com/karthiksijju' target='blank'>
 
-                </FormContainer>
+                    <BsInstagram/>
+                    </Link>
+                  </IconContainer>
+                  <IconContainer color='white' size='1.5rem'>
+                   <Link href='https://twitter.com/s0loLife' target='blank'>
+                    
+                    <BsTwitter/>
+                   </Link>
+                  </IconContainer>
+                  <IconContainer color='white' size='1.5rem'>
+                    <Link href='https://github.com/sijju' target='blank'>
+
+                    <BsGithub />
+                    </Link>
+                  </IconContainer>
+                  <IconContainer color='white' size='1.5rem'>
+                    <Link href='mailto:karthik.msv1234@gmail.com' target='blank'>
+
+                    <FiMail/>
+                    </Link>
+                  </IconContainer>
             </FlexContainer>
         </PaddingContainer>
+             
     </PaddingContainer>
   )
 }
